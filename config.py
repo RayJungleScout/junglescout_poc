d238400f73fde685ba0648249fc0fe048ea7e7f6
Ray Lin <ray.lin@junglescout.com>
2021-12-24 11:32:31 +0800

    update the requirement.txt(add python-dotenv); add WECHAT_WORK_TOKEN and WECHAT_WORK_AES_KEY to config; add the __init__ function to config;

--- config.py
+++ config.py
@@ -1,4 +1,18 @@
+import dotenv
+import os
 
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     REDIS_URL = "redis://localhost:6379/0"
+
+    WECHAT_WORK_TOKEN = "test"
+    WECHAT_WORK_AES_KEY = "test"
+
+    def __init__(self) -> None:
+        dotenv.load_dotenv(dotenv_path=os.path.join(PROJECT_PATH, '.env'))
+        for key, value in os.environ.items():
+            if hasattr(self, key):
+                if value in ['True', 'False']:
+                    value = eval(value)
+                setattr(self, key, value)
